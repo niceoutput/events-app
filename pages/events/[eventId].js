81eb1d23688f3afe6166ab10f6bc361fe7e3cf83
mario <mario@uniplaces.com>
2021-03-23 23:48:37 +0000

    bootstrap the project and create some structure

---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>EventDetailPage</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const AllEventsPage = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+    </div>
+  );
+};
+
+export default AllEventsPage;
