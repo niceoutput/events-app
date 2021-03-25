c88311f7012048ca02796c010f8c91ae23a9b01c
mario <mario@uniplaces.com>
2021-03-25 23:53:01 +0000

    add more components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  console.log(event);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>EventDetailPage</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
       <h1>Events Page</h1>
+      <EventList items={events} />
     </div>
   );
 };
